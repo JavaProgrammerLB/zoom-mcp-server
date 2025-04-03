95d0f7aa48f34b495b473b1bc52162cd9c64ea4b
Bill Lau <656860993@qq.com>
2025-04-03 10:28:31 +0800

    define response error type

--- common/error.ts
+++ common/error.ts
@@ -9,6 +9,13 @@ export class ZoomError extends Error {
   }
 }
 
+export class ZoomBadRequestError extends ZoomError {
+  constructor(message = "Bad request") {
+    super(message, 400, { message });
+    this.name = "ZoomBadRequestError";
+  }
+}
+
 export class ZoomAuthenticationError extends ZoomError {
   constructor(message = "Authentication failed") {
     super(message, 401, { message });
@@ -16,10 +23,30 @@ export class ZoomAuthenticationError extends ZoomError {
   }
 }
 
+export class ZoomNotFoundError extends ZoomError {
+  constructor(message = "Not Found") {
+    super(message, 404, { message });
+    this.name = "Not Found";
+  }
+}
+
+export class ZoomTooManyRequests extends ZoomError {
+  constructor(message = "Too Many Requests") {
+    super(message, 429, { message });
+    this.name = "Too Many Requests";
+  }
+}
+
 export function createZoomError(status: number, response: any): ZoomError {
   switch (status) {
+    case 400:
+      return new ZoomBadRequestError(response?.message);
     case 401:
       return new ZoomAuthenticationError(response?.message);
+    case 404:
+      return new ZoomNotFoundError(response?.message);
+    case 429:
+      return new ZoomTooManyRequests(response?.message);
     default:
       return new ZoomError(
         response?.message || "Zoom API error",
