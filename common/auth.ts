5abe429b11dc43871ba20be6b3d7e498ff86cc4c
Bill Lau <656860993@qq.com>
2025-04-03 11:25:37 +0800

    refactor

--- common/auth.ts
+++ common/auth.ts
@@ -2,14 +2,13 @@ import { TokenSchema } from "./types.js";
 import { zoomRequest } from "./util.js";
 
 export async function getAccessToken() {
-  let accountId = "";
-  if (process.env.ZOOM_ACCOUNT_ID) {
-    accountId = process.env.ZOOM_ACCOUNT_ID;
-  }
+  let accountId = process.env.ZOOM_ACCOUNT_ID
+    ? process.env.ZOOM_ACCOUNT_ID
+    : "";
 
   let authUrl = `https://zoom.us/oauth/token?grant_type=account_credentials&account_id=${accountId}`;
 
-  const key = `Basic ${calculateToken()}`;
+  const key = `Basic ${refreshToken()}`;
   const response = await zoomRequest(authUrl, {
     method: "POST",
     headers: { Authorization: key },
@@ -17,17 +16,11 @@ export async function getAccessToken() {
   return TokenSchema.parse(response);
 }
 
-function calculateToken(): string {
-  let clientId = "";
-  let clientSecret = "";
-
-  if (process.env.ZOOM_CLIENT_ID) {
-    clientId = process.env.ZOOM_CLIENT_ID;
-  }
-
-  if (process.env.ZOOM_CLIENT_SECRET) {
-    clientSecret = process.env.ZOOM_CLIENT_SECRET;
-  }
+function refreshToken(): string {
+  let clientId = process.env.ZOOM_CLIENT_ID ? process.env.ZOOM_CLIENT_ID : "";
+  let clientSecret = process.env.ZOOM_CLIENT_SECRET
+    ? process.env.ZOOM_CLIENT_SECRET
+    : "";
   return generateBasicAuth(clientId, clientSecret);
 }
 
