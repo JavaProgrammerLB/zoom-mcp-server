57319e41a467eeaa97d4d1e8060149c880a4af50
Bill Lau <656860993@qq.com>
2025-04-11 12:41:09 +0800

    complete param describtion

--- operations/meeting.ts
+++ operations/meeting.ts
@@ -7,8 +7,13 @@ export const CreateMeetingOptionsSchema = z.object({
     .string()
     .max(2000)
     .describe("The meeting's agenda")
-    .default("New Meeting"),
-  start_time: z.string().optional().describe("The meeting's start time"),
+    .default("New Meeting's agenda"),
+  start_time: z
+    .string()
+    .optional()
+    .describe(
+      "The meeting's start time. This supports local time and GMT formats.To set a meeting's start time in GMT, use the yyyy-MM-ddTHH:mm:ssZ date-time format. For example, 2020-03-31T12:02:00Z. To set a meeting's start time using a specific timezone, use the yyyy-MM-ddTHH:mm:ss date-time format and specify the timezone ID in the timezone field. If you do not specify a timezone, the timezone value defaults to your Zoom account's timezone. You can also use UTC for the timezone value. Note: If no start_time is set for a scheduled meeting, the start_time is set at the current time and the meeting type changes to an instant meeting, which expires after 30 days.",
+    ),
   timezone: z
     .string()
     .optional()
@@ -17,7 +22,13 @@ export const CreateMeetingOptionsSchema = z.object({
 });
 
 export const ListMeetingOptionsSchema = z.object({
-  type: z.string().optional().describe("The type of meeting."),
+  type: z
+    .string()
+    .optional()
+    .describe(
+      "The type of meeting. Choose from upcoming, scheduled or previous_meetings. upcoming - All upcoming meetings; scheduled - All valid previous (unexpired) meetings and upcoming scheduled meetings; previous_meetings - All the previous meetings;",
+    )
+    .default("upcoming"),
 });
 
 export const DeleteMeetingOptionsSchema = z.object({
